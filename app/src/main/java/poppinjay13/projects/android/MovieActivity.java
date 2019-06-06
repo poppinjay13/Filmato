bad1ee84762c68a064c17f051bea71a7b6152ed5
Ian Odundo <thisian13@gmail.com>
2019-06-06 23:50:41 +0300

    layout change and splash

--- app/src/main/java/poppinjay13/projects/android/MovieActivity.java
+++ app/src/main/java/poppinjay13/projects/android/MovieActivity.java
@@ -20,17 +20,19 @@ public class MovieActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.movies_recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
+        String description = "";
+        int rating = 5;
         List<MovieItem> modelClassList = new ArrayList<>();
-        modelClassList.add(new MovieItem(R.drawable.pic1,"Deadpool"));
-        modelClassList.add(new MovieItem(R.drawable.pic2,"Justice League"));
-        modelClassList.add(new MovieItem(R.drawable.pic3,"Pacific Rim Uprising"));
-        modelClassList.add(new MovieItem(R.drawable.pic4,"Aquaman"));
-        modelClassList.add(new MovieItem(R.drawable.pic5,"Mad Max Fury Road"));
-        modelClassList.add(new MovieItem(R.drawable.pic6,"Black Panther"));
-        modelClassList.add(new MovieItem(R.drawable.pic7,"Captain Marvel"));
+        modelClassList.add(new MovieItem(R.drawable.pic1,"Deadpool",description,rating));
+        modelClassList.add(new MovieItem(R.drawable.pic2,"Justice League",description,rating));
+        modelClassList.add(new MovieItem(R.drawable.pic3,"Pacific Rim Uprising",description,rating));
+        modelClassList.add(new MovieItem(R.drawable.pic4,"Aquaman",description,rating));
+        modelClassList.add(new MovieItem(R.drawable.pic5,"Mad Max Fury Road",description,rating));
+        modelClassList.add(new MovieItem(R.drawable.pic6,"Black Panther",description,rating));
+        modelClassList.add(new MovieItem(R.drawable.pic7,"Captain Marvel",description,rating));
 
         RecyclerView.Adapter adapter = new MovieAdapter(modelClassList);
         recyclerView.setAdapter(adapter);
---
+++ app/src/main/java/poppinjay13/projects/android/SplashScreen.java
@@ -0,0 +1,26 @@
+package poppinjay13.projects.android;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
