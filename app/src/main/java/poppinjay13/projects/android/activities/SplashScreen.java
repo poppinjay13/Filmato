e5e02ddaf660f9f8b650f9d219465eae44f61697
nicholus <nicholusmagak@gmail.com>
2019-07-07 23:53:08 +0300

    This is data on the localization

--- app/src/main/java/poppinjay13/projects/android/activities/SplashScreen.java
+++ app/src/main/java/poppinjay13/projects/android/activities/SplashScreen.java
@@ -1,9 +1,13 @@
 package poppinjay13.projects.android.activities;
 
 import android.content.Intent;
-import android.os.Handler;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 import poppinjay13.projects.android.R;
 
@@ -12,6 +16,12 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Locale locale = new Locale("sw-rKE");
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
 
